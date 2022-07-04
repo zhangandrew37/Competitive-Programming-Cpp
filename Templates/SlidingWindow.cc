95d3023bfdfabbe5b6a53932d5bf523a022333c0
zhangandrew37 <zhangandrew37@gmail.com>
2022-07-04 00:52:01 -0400

    add templates

---
+++ Templates/SlidingWindow.cc
@@ -0,0 +1,6 @@
+int l = 0;
+for (int r = 0; r < n; r++) {
+  // add the new thing you found at r
+  // while a condition you want to maintain is false, get rid of the thing at the left end and increase left by 1
+  // update the best you found with the current window size, which is guaranteed to be valid
+}
