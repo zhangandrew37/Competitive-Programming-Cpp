8e701cacc299f8db6a22174d88c0b24e9e2b533b
zhangandrew37 <zhangandrew37@gmail.com>
2022-07-04 19:06:54 -0400

    add: longestPalindromicSubstring

---
+++ Templates/STL.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// set arr elements to a value
+// Time: 
+// memset (void * ptr, int value, size_t num);
+    // ptr: pointer to block of memory to fill
+    // value: to set 
+    // num: num bytes to set to the value
+memset(arr, 0, sizeof(arr));
+
+// substr (void)
