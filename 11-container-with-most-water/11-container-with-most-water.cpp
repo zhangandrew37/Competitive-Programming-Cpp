fb827a0cc959ff8b34d991331082fd6a9fbd41ce
Andrew Zhang <54606438+zhangandrew37@users.noreply.github.com>
2022-07-08 12:09:28 -0400

    Time: 88 ms (98.27%), Space: 59.1 MB (37.73%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -5,6 +5,8 @@ public:
         int area = (r - l) * min(height[r], height[l]);
         while (l < r){
             if (height[l] >= height[r]){
+                // have while loop find next index to avoid unecessary calculations
+                //optimization: height[r-step] > height[r]
                 r--;
             } else {
                 l++;
