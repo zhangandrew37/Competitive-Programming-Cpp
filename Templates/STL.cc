94600c97245a8f56b556e12b5de7b45743920d94
zhangandrew37 <a298zhan@uwaterloo.ca>
2022-07-09 11:43:06 -0400

    add: unordered map

--- Templates/STL.cc
+++ Templates/STL.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <map>
-#include<vector>
+#include <vector>
+#include <utility
 
 // set arr elements to a value
 // Time: O(n)
@@ -16,10 +17,28 @@ memset(arr, 0, sizeof(arr));
 // ----  map (ordered) --- 
 map<string, int> m;
 
-//setting:
+//Use:
 
+// setting elements
+m["key"] = 1;
+// output:
+cout << m["keyNotInMap"] << endl; // outputs: 0  because if the key is not found, it is default constructed with value 0 (for int)
+// erase:
+m.erase ("key");
+// find/count (use find)
+if (m.count("def")) ... // 0 = not found, 1 = found
+if (m.find("key") != m.end()) ...
 
-// iterating
+// iterating (sorted by key order)
 for (auto &p : m){
+    // p's type is: pair<string, int>&
     cout << p.first << p.second << endl;
 }
+
+// ----  map (unordered) --- 
+unordered_map<int, int> m;
+
+// can iterate through key-value pairs (lambda function)
+
+// insert
+m.insert(pair<int, int>(nums[i], i));
