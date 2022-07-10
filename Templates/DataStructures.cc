5c820160b415e2d3e4d701014df3e98c7356ec40
zhangandrew37 <a298zhan@uwaterloo.ca>
2022-07-10 15:20:00 -0400

    update: stack

--- Templates/DataStructures.cc
+++ Templates/DataStructures.cc
@@ -56,13 +56,15 @@ while (head != nullptr){
 template<class Type, class container = deque<Type>> class stack;
 
 //ex: 
-stack<int> stack;
+stack<int> s;
 
-// empty() – Returns whether the stack is empty – Time Complexity : O(1) 
-// size() – Returns the size of the stack – Time Complexity : O(1) 
-// top() – Returns a reference to the top most element of the stack – Time Complexity : O(1) 
-// push(g) – Adds the element ‘g’ at the top of the stack – Time Complexity : O(1) 
-// pop() – Deletes the top most element of the stack – Time Complexity : O(1) 
+// Member functions: Time Complexity : O(1) 
+
+// empty() – Returns whether the stack is empty
+// size() – Returns the size of the stack 
+// top() – Returns a reference to the top most element of the stack
+// push(g) – Adds the element ‘g’ at the top of the stack 
+// pop() – Deletes the top most element of the stack (*** returns void, not top element)
 // swap() - 
 stack1.swap(stack2);
 // emplace() - Push but for creating elements on place
