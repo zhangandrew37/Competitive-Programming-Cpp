ecda84fb8f3323cb94d9a28ad3629b8eaf25611d
Andrew Zhang <54606438+zhangandrew37@users.noreply.github.com>
2022-07-16 22:58:29 -0400

    Time: 11 ms (62.34%), Space: 14.8 MB (81.00%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,7 +11,7 @@
 class Solution {
 public:
     
-    // remember memory management
+    // recursive
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if (!list1) return list2;
         if (!list2) return list1;
