b9b5ef1e48c39a2330aa38a672fc1d0ea5bbfa52
Andrew Zhang <54606438+zhangandrew37@users.noreply.github.com>
2022-08-12 22:33:34 -0400

    Time: 8 ms (96.65%), Space: 10.9 MB (47.70%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> m;
-        vector<int> res;
-
+        unordered_map<int, int> numsMap; 
+        vector<int> indices;
         for (int i = 0; i < nums.size(); i++){
             int diff = target - nums[i];
-            if (m.find(diff) != m.end()){
-                res.push_back(i);
-                res.push_back(m.at(diff));
+            if (numsMap.find(target - nums[i]) != numsMap.end()){ //using iterator design pattern
+                indices.push_back(i);
+                indices.push_back(numsMap.at(diff));
                 break;
             }
-            m.insert(pair<int, int>(nums[i], i));
+        numsMap[nums[i]] = i;
         }
-        return res;
+        return indices;
     }
 };
+
+/* Given: array of integers (nums), integer (target)
+- we want to find/return the indices of two numbers that add up to target
+
+Specifications
+- EXACTLY one solution
+- cannot use same element twice
+- can return in any order
+*/
+          
