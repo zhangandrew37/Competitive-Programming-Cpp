f74512ec8e97502ce123decef304b4077d7750d1
Andrew Zhang <54606438+zhangandrew37@users.noreply.github.com>
2022-08-20 13:51:26 -0400

    Time: 8 ms (11.91%), Space: 8.4 MB (23.98%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-         vector<int> bucket {0,0,0};
+         int r = 0, w = 0, b = 0;
         
         for (int n : nums){
-            if (n == 0) bucket[0]++;
-            if (n == 1) bucket[1]++;
-            if (n == 2) bucket[2]++;
+            if (n == 0) r++;
+            if (n == 1) w++;
+            if (n == 2) b++;
         }
         int i = 0;
-        while(bucket[0] > 0){
+        while(r > 0){
             nums[i++] = 0;
-            bucket[0]--;
+            r--;
         }
         
-        while (bucket[1] > 0){
+        while (w > 0){
             nums[i++] = 1;
-            bucket[1]--;
+            w--;
         }
 
-        while (bucket[2] > 0){
+        while (b > 0){
             nums[i++] = 2;
-            bucket[2]--;
+            b--;
         }
         
     }
