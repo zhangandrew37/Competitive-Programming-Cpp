4cb55f569ae7ca876660651a78bbd68ab4a0b173
Andrew Zhang <54606438+zhangandrew37@users.noreply.github.com>
2022-08-20 14:58:09 -0400

    Time: 3 ms (64.11%), Space: 8.3 MB (70.57%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-         int r = 0, w = 0, b = 0;
-        
-        for (int n : nums){
-            if (n == 0) r++;
-            if (n == 1) w++;
-            if (n == 2) b++;
-        }
-        int i = 0;
-        while(r > 0){
-            nums[i++] = 0;
-            r--;
-        }
-        
-        while (w > 0){
-            nums[i++] = 1;
-            w--;
-        }
-
-        while (b > 0){
-            nums[i++] = 2;
-            b--;
+        int len = nums.size();
+        int i = 0, l = 0, r = len - 1;
+        while (i <= r) {
+            if (nums[i] == 0){
+                swap(nums[i], nums[l]);
+                l++;
+                
+            } else if (nums[i] == 2){
+                swap(nums[i], nums[r]);
+                r--;
+                i--; // don't increment i
+            }
+            i++;
         }
         
     }
