bd1a732438191fd8c1538de1326af83b3d1fe4d5
Andrew Zhang <54606438+zhangandrew37@users.noreply.github.com>
2022-08-20 15:47:00 -0400

    Time: 0 ms (100.00%), Space: 6.4 MB (51.24%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> chars;
-        
         for(int i = 0 ; i < s.length(); i++){
             if (s[i] == '(' || s[i] == '[' || s[i] == '{'){
                 chars.push(s[i]);
@@ -10,18 +9,15 @@ public:
                 if (chars.empty()) return false;
                 if (s[i] == ')'){
                     if (chars.top() != '(') return false;
-                    chars.pop();
                 } else if (s[i] == ']'){
                     if (chars.top() != '[') return false;
-                    chars.pop();
                 } else {
                     if (chars.top() != '{') return false;
-                    chars.pop();
                 }
+                chars.pop();
             }
             }
         
-        if (!chars.empty()) return false;
-        return true;
+        return chars.empty();
     }
 };
