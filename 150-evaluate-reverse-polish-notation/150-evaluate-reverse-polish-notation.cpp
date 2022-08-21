106d1452a210bbbf1dd76dd5f3f5d8c22f7f6324
Andrew Zhang <54606438+zhangandrew37@users.noreply.github.com>
2022-08-21 13:57:05 -0400

    Time: 21 ms (33.88%), Space: 12 MB (19.62%) - LeetHub

---
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> nums;
+        for(string token: tokens){
+            if (token != "+" && token != "-" && token != "*" && token != "/"){
+                int n = stoi(token);
+                nums.push(n);
+            } else {
+                int first = nums.top();
+                nums.pop();
+                int second = nums.top();
+                nums.pop();
+                
+                if (token == "+"){
+                    nums.push(first + second);
+                } else if (token == "-"){
+                    nums.push(second - first);
+                } else if (token == "*"){
+                    nums.push(first * second);
+                } else {
+                    nums.push(second / first);
+                }
+            }
+        }
+        
+        return nums.top();
+    }
+};
