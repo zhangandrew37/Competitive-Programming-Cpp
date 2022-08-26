cce514b9378ebbc336f0993692dee904d1fe4062
Andrew Zhang <54606438+zhangandrew37@users.noreply.github.com>
2022-08-26 19:54:36 -0400

    Time: 22 ms (71.82%), Space: 7.8 MB (68.81%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,62 +1,37 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char,int> freq;
-        for(auto c:t)
-            freq[c]++;
-       
-        int i = 0;
-        int j = 0;
+        unordered_map<char, int> freqT;
+        int len = s.length();
+        int minLength = len + 1, minStart = 0;
+        for (char c: t){
+            freqT[c]++;
+        }
         
-        int minLength = INT_MAX;
-        int minStart = 0;
-        int counter = t.size();
-        while(j<s.size())
-        {
-            char curr = s[j];
-            if(freq[curr] > 0)          //if the curr char's freq is positive in the map then decrement counter 
-                counter--;               //this is because we we consume one unique char 
+        int l = 0, r = 0;
+        int counter = t.length();
+        
+        
+        while (r < len){
+            if (freqT[s[r]] > 0) counter--;
             
-            //we decrement the freq of curr char
-                //notice that its decremented for all char whether in map or not because for all char that are not in 't' freq                   //will be negative and will never be > 0!
+            freqT[s[r]]--;
+            r++;
             
-            freq[curr]--;               
-            
-            j++;                         
-            
-            //now we have consumed all the char in 't':
-                //we will shrink the length of the string and discard extra char that make the length longer
-                //this will true till counter == 0 because the moment we have counter > 0 we will know that we deleted 
-                // a char that was needed, so its not valid substring!
-            
-            while(counter == 0)
-            {
-                if(j-i < minLength)                 //update minlength for each interation 
-                {
-                    minLength = j-i;
-                    minStart = i;
-                }
-                char startChar = s[i];         
-                
-                //increment the freq of ith char 
-                freq[startChar]++;             
-                
-                //now we will check if the freq of ith char is positive or not
-                //notice that for all char which are not in 't' we will have -ve freq and now incrementing them will result to 
-                // 0 and not greater than 0
-                //the freq will be positive only for the char which are in 't' string!
-                // we increment the counter as we have an useful char to take in account!
-                if(freq[startChar] > 0)
-                {
-                    counter++;
+            while (counter == 0){
+                if (r - l < minLength){
+                    minLength = r-l;
+                    minStart = l;
                 }
                 
-                i++;
+                freqT[s[l]]++;
+                
+                if (freqT[s[l]] > 0) counter++;
+                l++;
             }
         }
-        if(minLength!=INT_MAX)
-            return s.substr(minStart,minLength);
-        else
-            return "";
+        
+        if (minLength == len + 1) return "";
+        return s.substr(minStart, minLength);
     }
 };
