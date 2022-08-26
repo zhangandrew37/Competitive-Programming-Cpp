8825bb48ece1690e901bfbf45a88fa99c623a8b2
Andrew Zhang <54606438+zhangandrew37@users.noreply.github.com>
2022-08-26 11:34:15 -0400

    Time: 206 ms (80.78%), Space: 89 MB (42.70%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -4,12 +4,12 @@ public:
         // we want the next greater than
         int len = temperatures.size();
         vector<int> answer(len, 0);
-        stack<int> nextWarmerIndices; // monotonic decreasing
+        stack<int> nextWarmerIndices; // monotonic decreasing, keep track of indices
         
         for (int i = 0; i < len; i++){
             while (!nextWarmerIndices.empty() && temperatures[i] > temperatures[nextWarmerIndices.top()]){
                 int popIndex = nextWarmerIndices.top();
-                answer[popIndex] = i - popIndex;
+                answer[popIndex] = i - popIndex; 
                 nextWarmerIndices.pop();
             }
             
