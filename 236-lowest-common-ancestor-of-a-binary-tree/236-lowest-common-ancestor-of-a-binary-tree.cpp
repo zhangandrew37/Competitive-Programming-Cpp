987c599355d47601f304ea8038215bca39f19ec5
Andrew Zhang <54606438+zhangandrew37@users.noreply.github.com>
2022-08-29 12:50:01 -0400

    Time: 42 ms (8.14%), Space: 14.4 MB (23.29%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,54 +9,14 @@
  */
 class Solution {
 public:
-    unordered_set<int> pSet;
-    stack<TreeNode*> qStack;
-    
-    bool searchP(TreeNode* root, TreeNode* p){
-        if (!root) return false;
-        
-        pSet.insert(root->val);
-        
-        if (root->val == p->val){
-            return true;
-        }
-        if (searchP(root->left, p) || searchP(root->right, p)) return true;
-        
-        pSet.erase(root->val);
-        return false;
-    }
-    
-    bool searchQ(TreeNode* root, TreeNode* q){
-        if (!root) return false;
-        
-        qStack.push(root);
-        
-        if (root->val == q->val){
-            return true;
-        }
-        if (searchQ(root->left, q) || searchQ(root->right, q)) return true;
-        
-        qStack.pop();
-        return false;
-    }
-    
-    
     
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        // find p, add to unordered set
-        searchP(root, p);
-        // find q, add to stack
-        searchQ(root, q);
-        // (while) loop through stack
-        for (auto c: pSet){
-            cout << c;
-        }
-        while (!qStack.empty()){
-            if (pSet.find(qStack.top()->val) != pSet.end()){
-                return qStack.top(); 
-            } 
-            qStack.pop();
-        }
-        return nullptr;
+        if (!root) return nullptr;
+        if (root == p || root == q) return root; // split this and above case for clarity
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (!left && !right) return nullptr;
+        if (left && right) return root;
+        return !left ? right : left;
     }
 };
