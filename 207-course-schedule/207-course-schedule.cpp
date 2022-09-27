d36721b9f1a10fdd2c1422656466811ee83edbd3
Andrew Zhang <54606438+zhangandrew37@users.noreply.github.com>
2022-09-27 00:02:57 -0400

    Time: 62 ms (11.91%), Space: 14 MB (54.34%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    unordered_map<int, vector<int>> adj;
-    unordered_set<int> visited;
     
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        // check for bi-directional edges, map <key, set?> (cycle)
-        // check if each node is < numCourses
+        vector<vector<int>> adj(numCourses, vector<int>());
+        vector<int> degree(numCourses, 0);
         
         // first make into adj list
         for(auto courses : prerequisites){
-            adj[courses[0]].push_back(courses[1]);
+            adj[courses[1]].push_back(courses[0]);
+            degree[courses[0]]++;
         }
         
-        // dfs, cur must be empty for it to return true
+        queue<int> q;
+        
         for (int i = 0; i < numCourses; i++){
-            if (!dfs(i)) return false;
+            if (degree[i] == 0) q.push(i); // add courses w/ empty adj list to queue
         }
         
-        return true;
-        
-    }
-    
-    bool dfs(int course){
-        if (visited.find(course) != visited.end()) return false;
-        if (adj[course].empty()) return true;
-        visited.insert(course);
-        for (auto pre : adj[course]){
-            if (dfs(pre) == false) return false;
+        while (!q.empty()){
+            int cur = q.front(); 
+            q.pop();
+            numCourses--;
+            
+            for (auto n : adj[cur]) {
+                if (--degree[n] == 0) q.push(n);
+            }
         }
-        visited.erase(course);
-        adj[course] = {};
-        return true;
+        
+        return numCourses == 0;
+        
     }
 };
