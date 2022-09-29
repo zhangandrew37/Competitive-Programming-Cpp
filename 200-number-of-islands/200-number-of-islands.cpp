daeb05bd6269297ac958514985d55226791377e0
Andrew Zhang <54606438+zhangandrew37@users.noreply.github.com>
2022-09-28 20:49:54 -0400

    Time: 122 ms (6.93%), Space: 18.2 MB (17.21%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -3,12 +3,11 @@ public:
     vector<int> dirs = {0, 1, 0, -1 , 0};
     int numIslands(vector<vector<char>>& grid) {
         int rows = grid.size(), cols = grid[0].size();
-        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
         int islands = 0;
         for (int r = 0; r < rows; r++){
             for (int c = 0; c < cols; c++){
-                if (grid[r][c] == '1' && !visited[r][c]){
-                    bfs(grid, visited, r, c);
+                if (grid[r][c] == '1'){
+                    bfs(grid, r, c);
                     islands++;
                 }
             }
@@ -17,18 +16,18 @@ public:
         return islands;
     }
     
-    void bfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int &r, int &c){
+    void bfs(vector<vector<char>>& grid, int &r, int &c){
         queue<pair<int, int>> q;
         q.push({r, c});
-        visited[r][c] = true;
+        grid[r][c] = 0;
         while (!q.empty()){
             int curRow = q.front().first, curCol = q.front().second;
             q.pop();
             for (int i = 0; i < 4; i++){
                 int newRow = curRow + dirs[i], newCol= curCol + dirs[i + 1];
                 if (newRow < 0 || newCol < 0 || newRow >= grid.size() 
-                    || newCol >= grid[0].size() || visited[newRow][newCol] || grid[newRow][newCol] != '1' ) continue;
-                visited[newRow][newCol] = true;
+                    || newCol >= grid[0].size() || grid[newRow][newCol] != '1' ) continue;
+                grid[newRow][newCol] = 0;
                 q.push({newRow, newCol});
             }
         }
