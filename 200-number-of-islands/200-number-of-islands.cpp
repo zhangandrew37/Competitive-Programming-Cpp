fe081b262aa4bedc6248f84a28f67a8a59792e05
Andrew Zhang <54606438+zhangandrew37@users.noreply.github.com>
2022-09-28 21:12:31 -0400

    Time: 31 ms (98.70%), Space: 12.3 MB (55.33%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -9,25 +9,23 @@ public:
             for (int c = 0; c < cols; c++){
                 if (grid[r][c] == '1'){
                     islands++;
-                    //bfs
-                    grid[r][c] = '0';
-                    queue<pair<int, int>> q;
-                    q.push({r, c});
-                    while (!q.empty()){
-                        int curRow = q.front().first, curCol = q.front().second;
-                        q.pop();
-                        for (int i = 0; i < 4; i++){
-                            int newRow = curRow + dirs[i], newCol = curCol + dirs[i+1];
-                            if (newRow < 0 || newCol < 0 || newRow >= rows || newCol >= cols 
-                                || grid[newRow][newCol] != '1') continue;
-                            grid[newRow][newCol] = '0';
-                            q.push({newRow, newCol});
-                        }
-                    }
+                    eraseIslands(grid, r, c);
                 }
             }
         }
         
         return islands;
+    }
+    
+    void eraseIslands(vector<vector<char>>&grid, int i, int j){
+        int m = grid.size(), n = grid[0].size();
+        if (i < 0 || i == m || j < 0 || j == n || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        eraseIslands(grid, i - 1, j);
+        eraseIslands(grid, i + 1, j);
+        eraseIslands(grid, i, j - 1);
+        eraseIslands(grid, i, j + 1);
     }
 };
