7ac7c7b41469cbbe01d8c5cbfc40869bbc89471b
Andrew Zhang <54606438+zhangandrew37@users.noreply.github.com>
2023-09-17 22:46:32 -0400

    Time: 27 ms (70.03%), Space: 27.8 MB (5.85%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -7,10 +7,12 @@ public:
             int m = l + (r-l)/2;
             
             if (nums[m] == target) return m;
-            else if (nums[m] < target) l = m + 1;
-            else r = m - 1;
+            else if (nums[m] < target) l++;
+            else r--;
         }
         
         return -1;
     }
 };
+
+// 3
