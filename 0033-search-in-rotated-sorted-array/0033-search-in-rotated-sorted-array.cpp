bd6cc468d8502ee44def80a332cfd851831706c2
Andrew Zhang <54606438+zhangandrew37@users.noreply.github.com>
2023-09-18 16:33:42 -0400

    Time: 3 ms (65.76%), Space: 11.6 MB (6.29%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -5,17 +5,13 @@ public:
         
         while (l < r){
             int m = (l + r) / 2;
-            if (nums[l] < nums[r]) {
-                if (nums[m] == target) return m;
-                else if (nums[m] < target) l = m + 1;
-                else r = m - 1;
-            } else if (nums[l] < nums[m]) { // first half sorted
+            if (nums[l] < nums[m]) { // first half sorted, include middle
                 if (nums[l] <= target && target <= nums[m]){
                     r = m;
                 } else {
                     l = m + 1;
                 }
-            } else { // second half sorted
+            } else { // second half sorted, exclude middle
                 if (nums[m + 1] <= target && target <= nums[r]){
                     l = m + 1;
                 } else {
