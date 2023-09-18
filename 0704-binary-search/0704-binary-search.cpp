84525fa11ee57c06fc331993cee834bfcb3e4dfd
Andrew Zhang <54606438+zhangandrew37@users.noreply.github.com>
2023-09-17 22:39:05 -0400

    Time: 27 ms (70.03%), Space: 27.8 MB (5.85%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        
+        while (l <= r){
+            int m = l + (r-l)/2;
+            
+            if (nums[m] == target) return m;
+            else if (nums[m] < target) l++;
+            else r--;
+        }
+        
+        return -1;
+    }
+};
+
+// 3
